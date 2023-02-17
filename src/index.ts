15198900ec4100c37d3a0c04f5218fd4e8f4f1e8
Paweł Kołaczyński <kolaczyn@protonmail.com>
2023-02-17 18:01:43 +0000

    split into files

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,10 @@
-import fs from 'fs/promises'
-import axios from 'axios'
+import { fetchPage } from './fetchPage'
+import { readWatchList } from './readWatchList'
 import { scrapePage } from './scrapePage/scrapePage'
 import { GameInfo } from './types'
 
 const formatInfo = (info: GameInfo) =>
   `${info.title}: z ${info.prev} na ${info.curr}`
-
-const XBOX_URL = 'https://www.microsoft.com/pl-pl/store/deals/games/xbox'
-const ITEMS_PER_AGE = 90
-
-const fetchPage = (page = 0) =>
-  Promise.resolve(`${XBOX_URL}?skipItems=${page * ITEMS_PER_AGE}`)
-    .then(url => axios.get<string>(url))
-    .then(resp => resp.data)
-
-const readWatchList = () =>
-  fs.readFile('./watchlist.txt').then(x => x.toString().split('\n'))
 
 const getAllGames = () =>
   Promise.all([0, 1, 2, 3].map(fetchPage)).then(x => x.flatMap(scrapePage))
@@ -26,10 +15,14 @@ const filterWatchList = async () => {
     getAllGames(),
   ])
 
-  allGames
+  const messages = allGames
     .filter(game => watchList.includes(game.title))
     .map(formatInfo)
-    .forEach(x => console.log(x))
+
+  if (messages.length) {
+    return messages.forEach(x => console.log(x))
+  }
+  return console.log('No games found')
 }
 
 filterWatchList()
