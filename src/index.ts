cb2fc47ff01ac8bba9cd64f1c8b4427b08ca38b1
Paweł Kołaczyński <kolaczyn@protonmail.com>
2023-02-17 19:09:08 +0000

    organize code

--- src/index.ts
+++ src/index.ts
@@ -7,24 +7,21 @@ import { readWatchList } from './readWatchList'
 import { scrapePage } from './scrapePage/scrapePage'
 import { GameInfo } from './types'
 
-const formatInfo = (info: GameInfo) =>
-  `${info.title}: z ${info.prev} na ${info.curr}`
-
 const getAllGames = () =>
   Promise.all([0, 1, 2, 3].map(fetchPage)).then(x => x.flatMap(scrapePage))
-
-const filterWatchList = async () => {
+const fetchWatchlistGamesOnPromotion = async () => {
   const [watchList, allGames] = await Promise.all([
     readWatchList(),
     getAllGames(),
   ])
 
-  const messages = allGames
-    .filter(game => watchList.includes(game.title))
-    .map(formatInfo)
-
-  return messages.length ? messages.join('\n') : 'No games found'
+  return allGames.filter(game => watchList.includes(game.title))
 }
+
+const formatInfo = (info: GameInfo) =>
+  `${info.title}: z ${info.prev} na ${info.curr}`
+const formatDiscordMessage = (games: GameInfo[]) =>
+  games.length > 0 ? games.map(formatInfo).join('\n') : 'No games found'
 
 /** @throws {Error} if env vars are not defined */
 const sendDiscordMessage = async (message: string) => {
@@ -34,7 +31,7 @@ const sendDiscordMessage = async (message: string) => {
     throw new Error(`id and/or token not defined. Can't send a message`)
 
   const webhookClient = new WebhookClient({ id, token })
-  console.log('sending message:', message)
+  console.log('sending message:\n', message)
   await webhookClient.send({
     content: message,
   })
@@ -42,7 +39,8 @@ const sendDiscordMessage = async (message: string) => {
 }
 
 const main = async () => {
-  const message = await filterWatchList()
+  const games = await fetchWatchlistGamesOnPromotion()
+  const message = formatDiscordMessage(games)
   sendDiscordMessage(message)
 }
 
