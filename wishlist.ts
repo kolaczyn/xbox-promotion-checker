a4d2713c70ef7678cb96f237013149c96d0b0ee5
Paweł Kołaczyński <kolaczyn@protonmail.com>
2023-02-21 19:46:48 +0100

    create threshold api

---
+++ wishlist.ts
@@ -0,0 +1,17 @@
+import { WishInput } from './src/types'
+
+export const wishlist: WishInput[] = [
+  {
+    title: 'The Witcher 2',
+    threshold: 20,
+  },
+  {
+    title: 'Sekiro™: Shadows Die Twice - edycja GOTY',
+    threshold: 100,
+  },
+  'Dziedzictwo Hogwartu',
+  'Crusader Kings III',
+  'Super Meat Boy',
+  'Super Meat Boy Forever',
+  'Cuphead',
+]
